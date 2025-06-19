ba197551d4ca06fcac78c2d7fd0ee469d7e603f7
Joliv3r <jacobobruun@gmail.com>
2025-06-19 22:40:03 +0200

    feat: shell files

---
+++ shell/README.md
@@ -0,0 +1,1 @@
+This is nothing but a collection of some shell files for development.
---
+++ shell/python.nix
@@ -0,0 +1,10 @@
+# https://nixos.wiki/wiki/Python
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      python-pkgs.numpy
+    ]))
+  ];
+}
