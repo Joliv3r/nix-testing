7e187a89ad6d8a38a329dda68dffd8baed92d64c
Joliv3r <jacobobruun@gmail.com>
2026-03-12 16:34:40 +0100

    feat: rewriting of master flakes

--- flake.nix
+++ flake.nix
@@ -5,25 +5,21 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs :
+  outputs = { nixpkgs, ... }:
   {
-    # The host with the hostname `my-nixos` will use this configuration
     nixosConfigurations.hilbert = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
       modules = [
         ./hilbert.nix
       ];
     };
 
     nixosConfigurations.hausdorff = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
       modules = [
         ./hausdorff.nix
       ];
     };
 
     nixosConfigurations.runningman = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
       modules = [
         ./runningman.nix
       ];
