f38b697e1bc7db1158da047ac35034e7ce631246
Joliv3r <jacobobruun@gmail.com>
2026-03-12 19:32:11 +0100

    chore: removed unnecessary comment

--- users/joliver.nix
+++ users/joliver.nix
@@ -2,7 +2,7 @@
 
 {
   programs.zsh.enable = true;
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+
   users.users.joliver = {
     isNormalUser = true;
     extraGroups = [ "wheel" ] # Enable ‘sudo’ for the user.
